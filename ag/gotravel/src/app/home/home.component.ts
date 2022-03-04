15405b1788e962728f393ed24d76d9b09300c762
Murload <almurcia46@misena.edu.co>
2022-03-04 12:16:51 -0500

    install sweet alert

--- ag/gotravel/src/app/home/home.component.ts
+++ ag/gotravel/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,15 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  alerta_inicio(){
+    Swal.fire(
+      'The Internet?',
+      'That thing is still around?',
+      'question'
+    )
+
+  }
+
 
 }
