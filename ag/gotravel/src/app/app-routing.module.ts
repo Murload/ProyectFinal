e16ff15ed328a9a39f551cfe885a0d8e84f11c30
sofíaN <91507303+SofiaNeira@users.noreply.github.com>
2022-03-11 12:33:59 -0500

    new component navbar

--- ag/gotravel/src/app/app-routing.module.ts
+++ ag/gotravel/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+
+// My imports
+import { HomeComponent } from './components/views/home/home.component';
+import { LoginComponent } from './components/views/login/login.component';
+import { MoneyConverterComponent } from './components/views/money-converter/money-converter.component';
+import { NanniesComponent } from './components/views/nannies/nannies.component';
+import { NewTripComponent } from './components/views/new-trip/new-trip.component';
+import { PublicTripComponent } from './components/views/public-trip/public-trip.component';
+import { RegisterComponent } from './components/views/register/register.component';
+import { TouristPlanComponent } from './components/views/tourist-plan/tourist-plan.component';
+
 const routes: Routes = [
-  {path : "", component:HomeComponent}
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'money', component: MoneyConverterComponent },
+  { path: 'nana', component: NanniesComponent },
+  { path: 'newtrip', component: NewTripComponent },
+  { path: 'publictrip', component: PublicTripComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'plan', component: TouristPlanComponent }
 ];
 
 @NgModule({
--- ag/gotravel/src/app/app.module.ts
+++ ag/gotravel/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './components/views/home/home.component';
 import { LoginComponent } from './components/views/login/login.component';
 import { RegisterComponent } from './components/views/register/register.component';
 import { NanniesComponent } from './components/views/nannies/nannies.component';
@@ -11,6 +11,8 @@ import { MoneyConverterComponent } from './components/views/money-converter/mone
 import { TouristPlanComponent } from './components/views/tourist-plan/tourist-plan.component';
 import { NewTripComponent } from './components/views/new-trip/new-trip.component';
 import { PublicTripComponent } from './components/views/public-trip/public-trip.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +24,8 @@ import { PublicTripComponent } from './components/views/public-trip/public-trip.
     MoneyConverterComponent,
     TouristPlanComponent,
     NewTripComponent,
-    PublicTripComponent
+    PublicTripComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
