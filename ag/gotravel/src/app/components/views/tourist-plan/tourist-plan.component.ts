0400f0351d41c0d39bb5b8051ef26f26840625e7
Murload <almurcia46@misena.edu.co>
2022-03-27 22:40:16 -0500

    list tourism plans check

--- ag/gotravel/src/app/components/views/tourist-plan/tourist-plan.component.ts
+++ ag/gotravel/src/app/components/views/tourist-plan/tourist-plan.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Tourimsplan } from 'src/app/models/PlanTouris';
+import { TouristplanService } from 'src/app/services/touristplan/touristplan.service';
 @Component({
   selector: 'app-tourist-plan',
   templateUrl: './tourist-plan.component.html',
   styleUrls: ['./tourist-plan.component.css']
 })
 export class TouristPlanComponent implements OnInit {
+  listTourismplan : Tourimsplan []=[]
+  
+  constructor( private tourismService : TouristplanService ) { }
 
-  constructor() { }
+
 
   ngOnInit(): void {
+    this.getPlans()
+  }
+  getPlans(){
+    this.tourismService.getPlans().subscribe(data =>{
+      // console.log(data);
+      this.listTourismplan = data;
+      console.log(this.listTourismplan);
+    },error =>{
+      console.log(error);
+    })
+
   }
 
 }
