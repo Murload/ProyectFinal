beb2b2cd0054e0c2b9934a33a071dbc09f99a06d
sofíaN <91507303+SofiaNeira@users.noreply.github.com>
2022-03-28 16:54:17 -0500

    nannas

--- ag/gotravel/src/app/services/nannies.service.ts
+++ ag/gotravel/src/app/services/nannies.service.ts
@@ -2,17 +2,44 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class NanniesService {
 
   urlApi = 'http://127.0.0.1:3000/api/nannies';
+  urlID = 'http://127.0.0.1:3000/api/nannies/id';
 
   constructor(private http: HttpClient) { }
 
   getNannies(): Observable<any>{
-    return this.http.get(this.urlApi)
+    const token = localStorage.getItem('token') || '';
+    return this.http.get(this.urlApi, {
+      headers: {
+        'x-token': token
+      }
+    })
   }
-}
 
+  getNanniesByCountry(count: String): Observable<any>{
+    const token = localStorage.getItem('token') || '';
+    return this.http.get(`${this.urlApi}/${count}`,{
+      headers: {
+        'x-token': token
+      }
+    })
+  }
+
+  getNanaById(id: any): Observable<any> {
+
+    const token = localStorage.getItem('token') || '';
+    return this.http.get(`${this.urlID}/${id}`,{
+      headers: {
+        'x-token': token
+      }
+    });
+  }
+
+}
